cc69eb76aa44d57aa647d3a27e61287f90af2173
danielufeli <danielufeli@yahoo.com>
2022-01-03 22:04:56 +0100

    Add localstorage and display book

--- main.js
+++ main.js
@@ -2,11 +2,24 @@ const container = document.querySelector('.container');
 const bookTitle = document.getElementById('title');
 const bookAuthor = document.getElementById('author');
 const btn = document.getElementById('btn-add');
-const arrBooks = [];
+const arrBooks = getBooks();
 
 function BookConstructor(title, author) {
   this.title = title;
   this.author = author;
+}
+
+function storeBooks(books) {
+  return localStorage.setItem('books', books);
+}
+
+function getBooks() {
+  let books = JSON.parse(localStorage.getItem('books'));
+  if (!books) {
+    books = [];
+    return books;
+  }
+  return books;
 }
 
 btn.addEventListener('click', () => {
@@ -18,13 +31,22 @@ btn.addEventListener('click', () => {
     inputs.value = '';
   });
   arrBooks.push(book);
+  storeBooks(JSON.stringify(arrBooks));
+  displayBooks();
+});
+
+function displayBooks() {
   const bookContainer = document.createElement('div');
   bookContainer.classList = 'books';
-  const bookContent = `<p class="title">${book.title}</p>
-    <p class="author">${book.author}</p>
+  let books = getBooks();
+  books.map((e, i) => {
+    const bookContent = `<p class="title">${books[i].title}</p>
+    <p class="author">${books[i].author}</p>
     <button id="btn-remove">Remove</button>
     <div class="line"></div>`;
-  bookContainer.innerHTML += bookContent;
-  container.appendChild(bookContainer);
-  console.log(arrBooks);
-});
+    bookContainer.innerHTML += bookContent;
+    container.appendChild(bookContainer);
+  });
+  console.log(books);
+}
+displayBooks();

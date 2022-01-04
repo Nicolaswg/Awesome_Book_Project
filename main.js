6bae3c57b919aeb1d38bdefc006cae521aef45c9
danielufeli <danielufeli@yahoo.com>
2022-01-05 00:07:52 +0100

    fix linter error

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-use-before-define */
 const bookTitle = document.getElementById('title');
 const bookAuthor = document.getElementById('author');
 const btn = document.getElementById('btn-add');
@@ -23,6 +22,35 @@ function getBooks() {
 
 let arrBooks = getBooks();
 
+const removeBook = (index) => {
+  arrBooks = arrBooks.filter((e, i) => i !== index);
+  storeBooks(JSON.stringify(arrBooks));
+};
+
+function displayBooks() {
+  let bookContent = '';
+  if (arrBooks.length === 0) {
+    document.querySelector('.books').innerHTML = 'No Books';
+  } else {
+    arrBooks.map((e, i) => {
+      bookContent += `<p class="title">${arrBooks[i].title}</p>
+  <p class="author">${arrBooks[i].author}</p>
+  <button class="remove">Remove</button>
+  <div class="line"></div>`;
+      document.querySelector('.books').innerHTML = bookContent;
+      const buttons = document.querySelectorAll('button.remove');
+      buttons.forEach((e, i) => {
+        buttons[i].addEventListener('click', (event) => {
+          removeBook(arrBooks.indexOf(arrBooks[i]));
+          displayBooks();
+          event.preventDefault();
+        });
+      });
+      return arrBooks;
+    });
+  }
+}
+
 function addBooks() {
   const title = bookTitle.value.toString();
   const author = bookAuthor.value.toString();
@@ -34,40 +62,6 @@ function addBooks() {
   arrBooks.push(book);
   storeBooks(JSON.stringify(arrBooks));
   displayBooks();
-}
-
-function createDisplay(bookContent) {
-  arrBooks.map((e, i) => {
-    bookContent += `<p class="title">${arrBooks[i].title}</p>
-  <p class="author">${arrBooks[i].author}</p>
-  <button class="remove">Remove</button>
-  <div class="line"></div>`;
-    document.querySelector('.books').innerHTML = bookContent;
-    const buttons = document.querySelectorAll('button.remove');
-    const removeBook = (index) => {
-      arrBooks = arrBooks.filter((e, i) => i !== index);
-      storeBooks(JSON.stringify(arrBooks));
-      // eslint-disable-next-line no-use-before-define
-      return displayBooks();
-    };
-    buttons.forEach((e, i) => {
-      buttons[i].addEventListener('click', (event) => {
-        // eslint-disable-next-line no-undef
-        removeBook(arrBooks.indexOf(arrBooks[i]));
-        event.preventDefault();
-      });
-    });
-    return arrBooks;
-  });
-}
-
-function displayBooks() {
-  const bookContent = '';
-  if (arrBooks.length === 0) {
-    document.querySelector('.books').innerHTML = 'No Books';
-  } else {
-    createDisplay(bookContent);
-  }
 }
 
 btn.addEventListener('click', () => {

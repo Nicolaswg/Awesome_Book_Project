44dc9e7df3c6f1b9b30a62c8db18e974badce39f
danielufeli <danielufeli@yahoo.com>
2022-01-05 00:12:08 +0100

    fix linter error

--- main.js
+++ main.js
@@ -64,41 +64,6 @@ function addBooks() {
   displayBooks();
 }
 
-<<<<<<< HEAD
-=======
-function createDisplay(bookContent) {
-  arrBooks.map((e, i) => {
-    bookContent += `<p class="title">${arrBooks[i].title}</p>
-  <p class="author">${arrBooks[i].author}</p>
-  <button class="remove">Remove</button>
-  <div class="line"></div>`;
-    document.querySelector('.books').innerHTML = bookContent;
-    const buttons = document.querySelectorAll('button.remove');
-    const removeBook = (index) => {
-      arrBooks = arrBooks.filter((e, i) => i !== index);
-      storeBooks(JSON.stringify(arrBooks));
-      return displayBooks();
-    };
-    buttons.forEach((e, i) => {
-      buttons[i].addEventListener('click', (event) => {
-        removeBook(arrBooks.indexOf(arrBooks[i]));
-        event.preventDefault();
-      });
-    });
-    return arrBooks;
-  });
-}
-
-function displayBooks() {
-  const bookContent = '';
-  if (arrBooks.length === 0) {
-    document.querySelector('.books').innerHTML = 'No Books';
-  } else {
-    createDisplay(bookContent);
-  }
-}
-
->>>>>>> a436f63976dd0a2e94d6570775796bb0138f7645
 btn.addEventListener('click', () => {
   addBooks();
 });
